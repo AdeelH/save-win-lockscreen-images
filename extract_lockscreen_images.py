2dde54b8fc15e0ee2752a815e586423bef77b4d7
Mahad Ahmed <herstalf@gmail.com>
2022-10-09 15:21:01 +0500

    Update extract_lockscreen_images.py

--- extract_lockscreen_images.py
+++ extract_lockscreen_images.py
@@ -2,6 +2,8 @@ import os
 import shutil
 from pathlib import Path
 from datetime import datetime
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 # There are usually a bunch of smaller images in the same place
 # that are not lockscreen images. We use this size threshold to detect them.
@@ -11,7 +13,7 @@ username = os.getlogin()
 src_dir = Path(f'C:/Users/{username}/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets') # yapf: disable
 curr_date = datetime.today()
 dst_dir = Path(
-    f'C:/Users/{username}/Pictures/lockscreen_images/{curr_date:%Y_%m_%d}')
+    f'C:/Users/{username}/Pictures/lockscreen_images/')
 
 if not os.path.exists(src_dir):
     raise FileNotFoundError(f'Source path {src_dir} not found. '
@@ -23,10 +25,17 @@ all_files = [f for f in src_dir.iterdir() if f.is_file()]
 large_files = [
     f for f in all_files if f.stat().st_size > FILE_SIZE_THRESHOLD_BYTES
 ]
-dst_dirs = [dst_dir / f.with_suffix('.jpg').name for f in large_files]
+
+landscape_files = []
+for f in large_files:
+    image = Image.open(f)
+    if image.width > image.height:
+        landscape_files.append(f)
+
+dst_dirs = [dst_dir / f.with_suffix('.jpg').name for f in landscape_files]
 
 print(f'Extracting {len(dst_dirs)} files to {dst_dir}...')
-for src, dst in zip(large_files, dst_dirs):
+for src, dst in zip(landscape_files, dst_dirs):
     shutil.copyfile(src, dst)
     print(f'Extracted: {dst.name}')
 os.startfile(dst_dir)
